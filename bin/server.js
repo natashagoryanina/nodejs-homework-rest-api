50f79792a92009f19fea2deb28ecf8dc06f8ff16
Natasha Goryanina <84124317+natashagoryanina@users.noreply.github.com>
2021-12-02 12:50:14 +0200

    avatars

--- bin/server.js
+++ bin/server.js
@@ -1,9 +1,60 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs').promises
+
+const app = express()
+//!
+const uploadDir = path.join(process.cwd(), 'public/uploads')
+const storeImage = path.join(process.cwd(), 'public/avatars')
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir)
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  },
+  limits: {
+    fileSize: 1024 * 1024,
+  },
+})
+
+const upload = multer({
+  storage: storage,
+})
+
+app.post('/upload', upload.single('picture'), async (req, res, next) => {
+  const { description } = req.body
+  const { path: temporaryName, originalname } = req.file
+  const fileName = path.join(storeImage, originalname)
+  try {
+    await fs.rename(temporaryName, fileName)
+  } catch (err) {
+    await fs.unlink(temporaryName)
+    return next(err)
+  }
+  res.json({ description, message: 'File uploaded successfully', status: 200 })
+})
+
+const isAccessible = (path) => {
+  return fs
+    .access(path)
+    .then(() => true)
+    .catch(() => false)
+}
+
+const createFolderIsNotExist = async (folder) => {
+  if (!(await isAccessible(folder))) {
+    await fs.mkdir(folder)
+  }
+}
+//!
+app.use(express.static(path.join(__dirname, '../public')))
 
 require('dotenv').config()
-const app = express()
 app.use(express.json())
 app.use(cors())
 
@@ -11,7 +62,7 @@ require('../config/config-passport')
 
 const contactsRouter = require('../routes/api/contacts')
 app.use('/api/contacts', contactsRouter)
-const { router } = require('../routes/api/user')
+const router = require('../routes/api/user')
 app.use('/api/users', router)
 
 app.use((req, res) => {
@@ -22,7 +73,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message })
 })
 
-const PORT = process.env.PORT || 3005
+const PORT = process.env.PORT || 3009
 const DB = process.env.DB_HOST
 
 mongoose.Promise = global.Promise
@@ -30,10 +81,18 @@ const connection = mongoose.connect(DB, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-
+//!
+// app.listen(PORT, async () => {
+//   createFolderIsNotExist(uploadDir)
+//   createFolderIsNotExist(storeImage)
+//   console.log(`Server running. Use on port:${PORT}`)
+// })
+//!
 connection
   .then(() => {
     app.listen(PORT, function () {
+      createFolderIsNotExist(uploadDir)
+      createFolderIsNotExist(storeImage)
       console.log(`Server running. Use our API on port: ${PORT}`)
     })
   })
